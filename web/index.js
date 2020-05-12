b42ba25ab2e772d25e3432242d5e5d96a8dfcf1a
fish <fishioon@live.com>
2020-05-12 23:14:33 +0800

    add docker-compose

--- web/index.js
+++ web/index.js
@@ -1,7 +1,7 @@
 const {ListConfigsRequest} = require('./xconf_pb.js');
 const {XconfClient} = require('./xconf_grpc_web_pb.js');
 
-var client = new XconfClient('http://127.0.0.1:8080', null, null);
+var client = new XconfClient('http://' + window.location.hostname + ':8080', null, null);
 
 var request = new ListConfigsRequest();
 request.setParent('ifish/rio');
